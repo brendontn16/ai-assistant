714c8bd2ae91b8bbb860a91d4bce4288feb33444
McKrak <89408346+McKrak@users.noreply.github.com>
2024-04-20 17:34:50 -0400

    Implemented animations

--- interface.py
+++ interface.py
@@ -1,45 +1,72 @@
 from tkinter import *
 import tkinter as tk
-from PIL import Image
+from PIL import Image, ImageTk
 
+sprites = { # Dictionary of all the sprites that can be loaded.
+  "idle": 'res/yalloIdle.png',
+  "yap": 'res/yalloYap.gif',
+  "banana": 'res/banana.png',
+}
+sprite_index = "yap" #Current sprite
 
 def getMeOut():
    exit()
+    
 
-class Overlay(tk.Tk):
+class Yallo(tk.Tk):
     def __init__(self, *a, **kw):
         tk.Tk.__init__(self, *a, **kw)
-        self._load_assets()
-        self._set_window_attrs()
-        self._draw()
+        self._load_assets() # Loads assets first (can be re-run)
+        self._set_window_attrs() # Configures window
+        self._draw() # Draws to the window
 
     def _load_assets(self):
-        self.spr_idle = tk.PhotoImage(file='res/yalloIdle.png')
+        img = Image.open(sprites[sprite_index])
+        self.anim_frames = []
+        self.anim_index = 0
+        self.anim_delay = 50
+        if img.format == "GIF":
+            for i in range(img.n_frames):
+                self.anim_frames.append(tk.PhotoImage(file = sprites[sprite_index], format = f"gif -index {i}"))
+        elif img.format == "PNG":
+            self.anim_frames.append(tk.PhotoImage(file = sprites[sprite_index]))
+        else:
+            self.anim_frames.append(tk.PhotoImage(file = sprites["banana"]))
+
 
     def _set_window_attrs(self):
         self.title("Overlay")
         screenWidth = self.winfo_screenwidth()
         screenHeight = self.winfo_screenheight()
-        self.geometry("{}x{}+{}+{}".format(screenWidth,screenHeight,0,0))
+        self.geometry("{}x{}+{}+{}".format(screenWidth, screenHeight, 0, 0))
         self.focus_force()
         self.wm_attributes("-topmost", True)
         self.overrideredirect(True)
 
         self.pos_x = 500
         self.pos_y = 250
+    
     def _draw(self):
         self.canvas = tk.Canvas(self, bg="green")
         self.canvas.pack(side="top", fill="both", expand=True)
         self.canvas.config(highlightthickness=0)
 
-        self.canvas.create_image(self.pos_x,self.pos_y,image=self.spr_idle,anchor="nw")
+        self.yallo = self.canvas.create_image(self.pos_x, self.pos_y, image=self.anim_frames[0], anchor="nw")
+        self.animate()
+        
         B = Button(self.canvas, text ="Escape", command = getMeOut)
-        B.place(x=self.pos_x-20,y=self.pos_y-10)
+        B.place(x=self.pos_x-20, y=self.pos_y-10)
 
         self.wm_attributes("-transparentcolor", "green")
+    
+    def animate(self):
+        self.anim_index = (self.anim_index + 1) % len(self.anim_frames)
+        self.canvas.itemconfig(self.yallo, image=self.anim_frames[self.anim_index])
+        self.after(self.anim_delay, self.animate)
+
     def run(self):
         self.mainloop()
     
 # driver code
 if __name__ == "__main__":
-    Overlay().run()
+    Yallo().run()
